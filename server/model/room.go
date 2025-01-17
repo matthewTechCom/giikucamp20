fbf1158415628136cf66b1ad8129be288e07a02a
Simetaro <sirime2351main@gmail.com>
2025-01-17 22:29:54 +0900

    created_atの追加

--- server/model/room.go
+++ server/model/room.go
@@ -1,11 +1,14 @@
 package model
 
+import "time"
+
 type Room struct {
-	ID          int     `json:"id"`
-	RoomImage   string  `json:"roomImage"`
-	RoomName    string  `json:"roomName"`
-	Password    string  `json:"password"`
-	Description string  `json:"description"`
-	Latitude    float64 `json:"latitude"`
-	Longitude   float64 `json:"longitude"`
+	ID          int       `json:"id"`
+	RoomImage   string    `json:"roomImage"`
+	RoomName    string    `json:"roomName"`
+	Password    string    `json:"password"`
+	Description string    `json:"description"`
+	Latitude    float64   `json:"latitude"`
+	Longitude   float64   `json:"longitude"`
+	CreatedAt   time.Time `json:"created_at"`
 }
