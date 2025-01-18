e3a6bcf34da40ba5e3ec4ac5a6fbcd311d4e418c
tmba <ut42.nu@gmail.com>
2025-01-18 12:51:25 +0900

    refactor format

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -12,8 +12,11 @@ const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLEMAP_API_KEY as string;
 
 const HomePage = () => {
   const { user, setUser } = useContext(UserContext);
-  const {isFirstViewModal, setIsFirstViewModal} = useMapTransactionContext();
-  const [clickLocation, setClickLocation] = useState<{ lat: number; lng: number } | null>(null);
+  const { isFirstViewModal, setIsFirstViewModal } = useMapTransactionContext();
+  const [clickLocation, setClickLocation] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
   const [containerStyle, setContainerStyle] = useState<React.CSSProperties>({
     width: "400px",
     height: "800px",
@@ -26,10 +29,15 @@ const HomePage = () => {
       const lng = event.latLng.lng();
       setClickLocation({ lat, lng }); // クリックした場所の緯度・経度を保存
       console.log("クリックした場所:", { lat, lng });
-      alert("クリックした場所は緯度が" + clickLocation?.lat + "緯度が" + clickLocation?.lng + "です")
+      alert(
+        "クリックした場所は緯度が" +
+          clickLocation?.lat +
+          "緯度が" +
+          clickLocation?.lng +
+          "です"
+      );
     }
   };
-
 
   useEffect(() => {
     const updateContainerStyle = () => {
@@ -88,38 +96,48 @@ const HomePage = () => {
 
   return (
     <>
-    <div className="flex justify-center min-h-screen bg-gray-100">
-      <div className="rounded shadow-md w-96 ">
-      {isFirstViewModal && (<FirstView />)}
-        <div className="flex p-4 bg-black bg-opacity-80 text-slate-100 justify-between">
-        <h1 className="text-2xl font-bold ">MapChat</h1>
-        <Image
-          src={user.userIcon || "/default-icon.png"}
-          alt="ユーザーアイコン"
-          width={36}
-          height={36}
-          className="rounded-full"
-        />
-        </div>
-        <div className="flex items-center justify-center">
-          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17} onClick={handleMapClick}>
-            <Marker
-              position={positionAkiba}
-              label={"秋葉原"}
-              icon={{
-                url: "https://lamp-ramp.com/wp-content/uploads/2023/08/keii01-1024x1024.png",
-                scaledSize: new google.maps.Size(50, 50), // `google` オブジェクトが存在する状態で実行
-              }}
-              onClick={handleChangeURL}
+      <div className="flex justify-center min-h-screen bg-gray-100">
+        <div className="rounded shadow-md w-96 ">
+          {isFirstViewModal && <FirstView />}
+          <div className="flex p-4 bg-black bg-opacity-80 text-slate-100 justify-between">
+            <h1 className="text-2xl font-bold ">MapChat</h1>
+            <Image
+              src={user.userIcon || "/default-icon.png"}
+              alt="ユーザーアイコン"
+              width={36}
+              height={36}
+              className="rounded-full"
             />
-            <Marker position={center} label={"テスト1"} />
-          </GoogleMap>
-        </div>
-        <div className="absolute bottom-2 left-70">
-          <button className="rounded-full bg-yellow-300 p-5 font-bold" onClick={() => setIsFirstViewModal(true)}>＋</button>
+          </div>
+          <div className="flex items-center justify-center">
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={17}
+              onClick={handleMapClick}
+            >
+              <Marker
+                position={positionAkiba}
+                label={"秋葉原"}
+                icon={{
+                  url: "https://lamp-ramp.com/wp-content/uploads/2023/08/keii01-1024x1024.png",
+                  scaledSize: new google.maps.Size(50, 50), // `google` オブジェクトが存在する状態で実行
+                }}
+                onClick={handleChangeURL}
+              />
+              <Marker position={center} label={"テスト1"} />
+            </GoogleMap>
+          </div>
+          <div className="absolute bottom-2 left-70">
+            <button
+              className="rounded-full bg-yellow-300 p-5 font-bold"
+              onClick={() => setIsFirstViewModal(true)}
+            >
+              ＋
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
