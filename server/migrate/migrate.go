ebaa240718ad4117c4b4876bd310b1b832b116b6
tmba <ut42.nu@gmail.com>
2025-01-19 01:38:47 +0900

    .envファイルのパスをコマンドラインフラグから取得

--- server/migrate/migrate.go
+++ server/migrate/migrate.go
@@ -3,21 +3,33 @@ package main
 import (
 	"chat_upgrade/db"
 	"chat_upgrade/model"
+	"flag"
 	"fmt"
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+    // コマンドラインフラグからenvファイルのパスを取得（デフォルトは ".env"）
+    envFilePath := flag.String("env", ".env", "Path to the .env file")
+    flag.Parse()
 
-	// データベース接続
-	dbConn := db.NewDB()
+    // envファイルの読み込み
+    err := godotenv.Load(*envFilePath)
+    if err != nil {
+        log.Fatalf("Error loading .env file from path %s: %v",*envFilePath, err)
+    }
 
-	// defer で順序指定
-	defer fmt.Println("Successfully Migrated")
-	defer db.CloseDB(dbConn)
+    // データベース接続
+    dbConn := db.NewDB()
 
-	// マイグレーションの実行とエラーハンドリング
-	if err := dbConn.AutoMigrate(&model.User{}, &model.Room{}); err != nil {
-		log.Fatalf("Failed to run migrations: %v\n", err)
-	}
+    // defer で順序指定
+    defer fmt.Println("Successfully Migrated")
+    defer db.CloseDB(dbConn)
+
+    // マイグレーションの実行とエラーハンドリング
+    if err := dbConn.AutoMigrate(&model.User{}, &model.Message{}, &model.Room{}); err != nil {
+        log.Fatalf("Failed to run migrations: %v\n", err)
+    }
 }
